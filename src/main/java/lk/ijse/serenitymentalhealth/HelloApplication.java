15f8e62d892ebe7467db4e5ead6e4d73d70a9a65
Chathura Rathanayake <chathurarathnayake443@gmail.com>
2026-04-26 17:18:50 +0530

    created login UI

--- src/main/java/lk/ijse/serenitymentalhealth/HelloApplication.java
+++ src/main/java/lk/ijse/serenitymentalhealth/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 700);
+        stage.setTitle("Serenity - Login");
         stage.setScene(scene);
         stage.show();
     }
