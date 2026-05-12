77848e556cfb8731b0cb5009ea00e53920380c82
Chathura Rathanayake <chathurarathnayake443@gmail.com>
2026-05-12 17:07:26 +0530

    set name to therapy program ui

--- src/main/java/lk/ijse/serenitymentalhealth/controller/TherapyProgramController.java
+++ src/main/java/lk/ijse/serenitymentalhealth/controller/TherapyProgramController.java
@@ -1,4 +1,52 @@
 package lk.ijse.serenitymentalhealth.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class TherapyProgramController {
+
+    @FXML
+    private TableColumn editCol;
+
+    @FXML
+    private ComboBox programComboBox;
+
+    @FXML
+    private TextField programCostField;
+
+    @FXML
+    private TextField programDescriptionField;
+
+    @FXML
+    private TextField programDurationField;
+
+    @FXML
+    private TableColumn programIdCol;
+
+    @FXML
+    private TextField programIdField;
+
+    @FXML
+    private TableColumn programNameCol;
+
+    @FXML
+    private TextField programNameField;
+
+    @FXML
+    private TableView programTbl;
+
+    @FXML
+    private ComboBox therapistIdComboBox;
+
+    @FXML
+    private TextField therapistIdField;
+
+    @FXML
+    private TableColumn therapistNameCol;
+
+    @FXML
+    private TableView therapistProgramTbl;
 }
