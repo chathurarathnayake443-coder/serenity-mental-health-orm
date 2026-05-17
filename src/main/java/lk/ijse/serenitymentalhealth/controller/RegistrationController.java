7b70cd04275eb6611d02bc5fc4fba650d502da60
Chathura Rathanayake <chathurarathnayake443@gmail.com>
2026-05-17 22:34:30 +0530

    setted registration ui ids

--- src/main/java/lk/ijse/serenitymentalhealth/controller/RegistrationController.java
+++ src/main/java/lk/ijse/serenitymentalhealth/controller/RegistrationController.java
@@ -1,4 +1,40 @@
 package lk.ijse.serenitymentalhealth.controller;
 
-public class RegistrationController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegistrationController implements Initializable {
+
+    @FXML
+    private DatePicker dateBox;
+
+    @FXML
+    private TextField feeField;
+
+    @FXML
+    private ComboBox nameBox;
+
+    @FXML
+    private TableColumn patientCol;
+
+    @FXML
+    private TextField patientNameField;
+
+    @FXML
+    private TableColumn paymentCol;
+
+    @FXML
+    private TableView registerTbl;
+
+    @FXML
+    private ComboBox smallNameBox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Registration View Loaded");
+    }
 }
