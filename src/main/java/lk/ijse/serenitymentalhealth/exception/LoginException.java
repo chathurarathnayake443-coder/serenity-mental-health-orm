68bb6fac48ac66a2df753ece2ba7d0289c3389a7
Chathura Rathanayake <chathurarathnayake443@gmail.com>
2026-06-05 13:25:00 +0530

    added custom exception classes

---
+++ src/main/java/lk/ijse/serenitymentalhealth/exception/LoginException.java
@@ -0,0 +1,7 @@
+package lk.ijse.serenitymentalhealth.exception;
+
+public class LoginException extends Exception{
+    public LoginException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/lk/ijse/serenitymentalhealth/exception/TherapySessionException.java
@@ -0,0 +1,7 @@
+package lk.ijse.serenitymentalhealth.exception;
+
+public class TherapySessionException extends Exception{
+    public TherapySessionException(String message){
+        super(message);
+    }
+}
